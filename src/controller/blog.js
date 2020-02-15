893508ea89ae6c739f6c0fb2cb8cf409f9ef48ba
panxiandiao_i <panxiandiao_i@didiglobal.com>
2020-02-15 13:32:06 +0800

    接口都已吊桶  通宵下载完了mysql&workbench

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -11,6 +11,40 @@ const getList = (author, keyword) => {
   ]
 }
 
+const getDetail = (id) => {
+  return [
+    {
+      id: 1,
+      title: '标题A'
+    },
+    {
+      id: 2,
+      title: '标题B'
+    }
+  ]
+}
+
+const newBlog = (blogData = {}) => {
+  return {
+    id: 1
+  }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  // id 为更新博客的id
+  // blogData 是一个博客对象， 包含 title content 属性
+  return true
+}
+
+const delBlog = (id) => {
+  // id 为删除博客的id
+  return true
+}
+
 module.exports = {
-  getList
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
 }
--- src/router/user.js
+++ src/router/user.js
@@ -1,10 +1,17 @@
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 const handleUserRouter = (req,res) => {
-  const method = req.methd // GET POST
+  const method = req.method // GET POST
 
   // 登陆
-  if(method === 'POST' && req.path === '/api/blog/login') {
-    return {
-      msg: '这里是登陆的接口'
+  if(method === 'POST' && req.path === '/api/user/login') {
+    const { username, password } = req.body
+    const result = loginCheck(username, password)
+    if(result) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel('登陆失败')
     }
   }
 
